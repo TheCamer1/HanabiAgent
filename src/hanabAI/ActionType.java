3017a43c39fc4475258bc167dc8f9a4740b8fa65
TheCamer1 <31721788+TheCamer1@users.noreply.github.com>
2019-11-22 17:57:11 +0800

    Delete ActionType.java

--- src/hanabAI/ActionType.java
+++
@@ -1,7 +0,0 @@
-package hanabAI;
-/**
- * An enumeration of the different types of actions a player can do in the game Hanabi.
- **/ 
-public enum ActionType{PLAY,DISCARD,HINT_COLOUR,HINT_VALUE};
-
-
